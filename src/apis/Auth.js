5ed6ceaf7e8c32a1aa504433fd35001ebc260e10
LeeoSilva <leeosilva0909@gmail.com>
2020-10-18 02:01:21 -0300

    apis documented

--- src/apis/Auth.js
+++ src/apis/Auth.js
@@ -1,20 +1,40 @@
 import Api from './Api';
 
 export default {
+    /**
+     * Authenticates the user.
+     *
+     * @param {Object} loginPayload
+     * @return {Promise<AxiosResponse>}
+     */
     login(loginPayload) {
         return Api.post('auth/login', loginPayload);
     }
 
+    /**
+     * Invalidates the token of the user.
+     *
+     * @return {Promise<AxiosResponse>} 
+     */
     logout() {
         return Api.post('auth/logout');
     }
 
+    /**
+     * Refreshes the token of the user
+     *
+     * @return {Promise<AxiosResponse>} 
+     */
     refresh() {
         return Api.post('auth/refresh');
     }
 
+    /**
+     * Gets the user info that's currently authenticated.
+     *
+     * @return {Promise<AxiosResponse>} 
+     */
     me(){
         return Api.get('auth/me');
     }
-
 }
--- src/apis/Contacts.js
+++ src/apis/Contacts.js
@@ -1,22 +1,52 @@
 import Api from './Api';
 
 export default {
+    /**
+     * Gets all the contacts from the user.
+     *
+     * @return {AxiosResponse}
+     */
     all() {
         return Api.get('contacts');
     }
 
+    /**
+     * Creates a contact in the database linked to the logged user.
+     *
+     * @param {Object} storeContactsPayload
+     * @return {Promise<AxiosResponse>} 
+     */
     create(storeContactsPayload) {
         return Api.post('contacts', storeContactsPayload);
     }
 
-    get() {
+    /**
+     * Gets a contact given his id.
+     *
+     * @param {int} id
+     * @return {Promise<AxiosResponse>} 
+     */
+    get(id) {
         return Api.get(`contacts/${id}`);
     }
 
-    update(updateContactsPayload){
+    /**
+     * Updates a contact information given his id and the update payload
+     *
+     * @param {int} id
+     * @param {Object} updateContactsPayload
+     * @return {Promise<AxiosResponse>}
+     */
+    update(id, updateContactsPayload){
         return Api.put(`contacts/${$id}`, updateContactsPayload);
     }
 
+    /**
+     * Deletes a contact given his id.
+     *
+     * @param {int} id
+     * @return {Promise<AxiosResponse} 
+     */
     delete(id){
         return Api.delete(`contacts/${id}`)
     }
